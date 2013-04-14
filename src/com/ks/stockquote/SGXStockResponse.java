692f1d6e2d6d4e878f8867e3e9ec86fa8c243cf3
ks <user1@HOME2>
2013-04-13 16:02:56 -0800

    20130413

--- src/com/ks/stockquote/SGXStockResponse.java
+++ src/com/ks/stockquote/SGXStockResponse.java
@@ -7,5 +7,13 @@ public class SGXStockResponse {
 	public String identifier;
 	public String label;
 	public ArrayList<SGXStockRecord> items;
+	
+	// "As at 12-04-2013 5:04 PM"
+	public static final String DEFAULT_LABEL = "As at 01-01-2013 12:00 AM";
+	
+	public SGXStockResponse()
+	{		
+		label = DEFAULT_LABEL;
+	}
 
 }
